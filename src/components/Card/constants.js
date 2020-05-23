e51c2a0d7580d9b68bbc93bda9e0dbc0a38c0e53
Daniel Feliz <47199869+dfeliz@users.noreply.github.com>
2020-05-23 12:50:37 -0400

    Update constants.js

--- src/components/Card/constants.js
+++ src/components/Card/constants.js
@@ -1,6 +1,6 @@
 export default {
   TEXT: {
     GREETING: "Hello there!",
-    PARAGRAPH: "I'm Daniel, a Computing Systems Engineering student that loves food, gaming and learning new stuff. I'm currently working at Soluciones GBH as a Software Developer.",
+    PARAGRAPH: "I'm Daniel, a Computing Systems Engineering student that loves food, gaming and learning new stuff. I'm currently working at Soluciones GBH as a Software Engine.",
   },
 };
