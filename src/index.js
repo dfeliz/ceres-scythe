34795863c0703a189ca9318b125447e9b1dc605a
dfeliz <nonifeliz1@gmail.com>
2021-11-01 13:25:22 -0400

    move inspectlet to recommended spot

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import runAnimation from './extras/animation';
-import runInspectlet from './extras/inspectlet';
 import * as serviceWorker from './serviceWorker';
 
-runInspectlet();
 runAnimation();
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
-serviceWorker.register();
+serviceWorker.unregister();
